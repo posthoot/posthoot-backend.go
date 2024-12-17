e8972a6b364c9c7782d8d236c48c3ad764a6f553
iamharshdev <me@iamharsh.dev>
2024-12-17 16:46:24 +0530

    🔧 feat(utils): add fetch and parser utilities

---
+++ internal/utils/fetch.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"io"
+	"net/http"
+)
+
+func GetHTMLFromURL(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
