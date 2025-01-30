b1d9a00d13be57440fa54b9c74ba8d7c67452cd1
iamharshdev <me@iamharsh.dev>
2025-01-30 10:25:16 +0530

    🔧 chore: Prevent duplicate role creation in PostgreSQL initialization
    - Add conditional role creation check in init-user-db.sh
    - Use PL/pgSQL DO block to check role existence before creation
    - Prevent potential errors when role already exists
    - Maintain idempotent database initialization script

--- docker-entrypoint-initdb.d/init-user-db.sh
+++ docker-entrypoint-initdb.d/init-user-db.sh
@@ -3,7 +3,14 @@ set -e
 
 echo "🔍 Creating PostgreSQL role..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE ROLE ${POSTGRES_USER} WITH LOGIN PASSWORD '${POSTGRES_PASSWORD}' CREATEDB;
+    DO
+    $do$
+    BEGIN
+        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '${POSTGRES_USER}') THEN
+            CREATE ROLE ${POSTGRES_USER} WITH LOGIN PASSWORD '${POSTGRES_PASSWORD}' CREATEDB;
+        END IF;
+    END
+    $do$;
     GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};
 EOSQL
 
