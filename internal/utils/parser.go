74a8203b351940b64f643acdb83dbe18c37a26f2
theboringhumane <me@iamharsh.dev>
2025-04-01 22:20:55 +0530

    🔧 fix: Add unsubscribe link to email content for improved user experience
    - Introduce functionality to append an unsubscribe link before the closing body tag in email HTML.
    - Utilize strings package to replace the closing body tag with the unsubscribe link, enhancing compliance and user control over email subscriptions.

--- internal/utils/parser.go
+++ internal/utils/parser.go
@@ -7,6 +7,7 @@ import (
 	"kori/internal/utils/base64"
 	"kori/internal/utils/logger"
 	"regexp"
+	"strings"
 
 	"github.com/golang-jwt/jwt"
 	"gorm.io/datatypes"
@@ -41,6 +42,7 @@ func ReplaceVariables(input string, variables map[string]string, mailId string, 
 	if trackLinks {
 		input = ReplaceLinksWithRedirect(input, mailId, cfg)
 	}
+
 	return base64.EncodeToBase64(input)
 }
 
@@ -83,6 +85,9 @@ func ReplaceLinksWithRedirect(html string, mailId string, cfg *config.Config) st
 
 	// Add tracking pixel at bottom of email to track opens
 	html = html + fmt.Sprintf(`<img src="%s/track/open?token=%s" style="display:none" width="1" height="1">`, cfg.Server.PublicURL, tokenString)
+
+	// add unsubcribe link to the input this needs to go before the closing body tag
+	html = strings.Replace(html, "</body>", fmt.Sprintf(`<table><tr><td><a href="%s/track/unsubscribe?token=%s">Unsubscribe from this list</a></td></tr></table></body>`, cfg.Server.PublicURL, tokenString), 1)
 
 	return html
 }
