80781293de1b40d7095de5fab91b8ce83e94db3a
theboringhumane <me@iamharsh.dev>
2025-04-10 15:08:28 +0530

    🔧 fix: Update Google OAuth redirect URL environment variable
    - Changed the environment variable name from GOOGLE_REDIRECT_URL to GOOGLE_REDIRECT_URI for consistency with Google API standards.
    - Ensured proper retrieval of the redirect URI in the Google OAuth configuration.

--- internal/utils/google.go
+++ internal/utils/google.go
@@ -15,7 +15,7 @@ const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_
 var googleOauthConfig = &oauth2.Config{
 	ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 	ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-	RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
+	RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URI"),
 	Scopes:       []string{"email", "profile"},
 }
 
